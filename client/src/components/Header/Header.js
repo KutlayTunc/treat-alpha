d81e33eb2fe9b70a0609c1937998175fc9d6be93
KutlayTunc <kutlay-tunc@hotmail.com>
2022-07-03 16:21:22 +0300

    added multilanguage

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import Logo from "../Logo/Logo"
 import Search from "../Search/Search"
+import { useTranslation } from "react-i18next"
 import "./Header.css"
 // import logo from "../../assets/logo.svg"
 const Header = () => {
+  const { t } = useTranslation()
   return (
     <div className="header-container">
       <Logo />
-      <div className="header-item">Current Page Message</div>
-
+      <div className="header-item">{t("welcomePageMessage")} Erol Egemen</div>
       <Search />
     </div>
   )
