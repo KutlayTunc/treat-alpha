4d288b391c11971f3fe75f1a0337465b1ee6f5a0
KutlayTunc <kutlay-tunc@hotmail.com>
2022-07-03 16:40:50 +0300

    added multilanguage

--- client/src/i18nextConf.js
+++ client/src/i18nextConf.js
@@ -2,7 +2,7 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import Backend from "i18next-xhr-backend"
 import LanguageDetector from "i18next-browser-languagedetector"
-import translationEN from "./assets/locales/en/translations.json"
+import translationEN from "./assets/locales/en/translation.json"
 
 const fallbackLng = ["en"]
 const availableLanguages = ["en", "ar", "fr"]
@@ -13,11 +13,12 @@ const resources = {
 }
 
 i18n
-  .use(Backend) // load translations using http (default                                               public/assets/locals/en/translations)
+  .use(Backend) // load translations using http (default: public/assets/locals/en/translations)
   .use(LanguageDetector) // detect user language
   .use(initReactI18next) // pass the i18n instance to react-i18next.
   .init({
     resources,
+
     fallbackLng, // fallback language is english.
 
     detection: {
@@ -27,10 +28,6 @@ i18n
     debug: false,
 
     whitelist: availableLanguages,
-
-    interpolation: {
-      escapeValue: false, // no need for react. it escapes by default
-    },
   })
 
 export default i18n
